519bb7e8ece0ebe08c82d01e10463d1c8d3377b0
BalaChi <73782089+BalaChi@users.noreply.github.com>
2021-09-07 20:30:32 +0530

    Add files via upload

---
+++ notes.py
@@ -0,0 +1,14 @@
+def notes_input():
+    """
+    Gets notes from user and prints it in the console
+
+   returns notes
+    """
+    notes = input("Enter your notes:")
+    print(notes)
+    return notes
+notes_input()
+
+
+
+
